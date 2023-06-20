bde78d484fa6ae45885539d14d40fde298ca84fa
Jagriti Verma <100408547+Jagriti027@users.noreply.github.com>
2023-06-20 11:54:15 +0530

    Add files via upload

---
+++ classes/class-2.js
@@ -0,0 +1,85 @@
+// const prompt=required("prompt-sync")({signt:true});
+// let n = prompt("Enter number");
+// for( let i = 1; i<=n ; i++){
+//    console.log(i);
+// }
+
+// referenceError : prompt is not defined - npm install prompt-sync
+
+// console.log(+'200');
+// 200 - number
+
+// console.log(+false)
+// 0 - number representaion
+
+// console.log(+"hello")
+// NaN
+
+// console.log(32+16);
+
+// const score = 80;
+// const scoreRating = score > 50 ?  "Good" : "Poor";
+// console.log(scoreRating);
+
+// Print FizzBuzz
+//for (let i = 1; i <= 100; i++) {
+//  let f = 1 % 3 == 0,
+//    b = i % 5 == 0;
+//  console.log(f ? (b ? "FizzBuzz" : "Fizz") : b ? "Buzz" : i);
+// }
+
+// STRINGS
+// let name = "SNEHA";
+//    for(let i=0; i<name.length; i++ ){
+//        console.log(name[i]);
+// }
+// console.log(name[0]);
+
+// STRING INTERPOLATION
+// const fakeName = "Sneha";
+
+// const message = ` Hello, ${fakeName} !`;
+// console.log(message);
+
+// ESCAPE SEQUENCE CHARACTER
+// let name1 = 'sneha';
+// let name = 'SNEHA\`S'
+// console.log(name);
+
+// let s1 = 'Javascript is a VERY Popular Language';
+// let s2 = s1.toLowerCase();
+// console.log(s2);
+
+// var s1 = 'sneha kuamri';
+// var s2 = s1.slice(0,4);
+// var s3 = s1.slice(5);
+// console.log(s2,s3);
+
+// FUNCTIONS
+
+// function functionName([arg1, arg2, ...argN]){
+// code to be executed
+// }
+
+// function cube(a){
+//    return a*a*a;
+// }
+// console.log(cube(5));
+
+// function product(a,b){
+//    console.log(a*b);
+// }
+// product(4,5);
+
+// function that returns product of two numbers
+// call()
+// function product(a,b){
+//    return a*b;
+// }
+
+// calling product() function
+// var result = product.call(this, 20, 5);
+// console.log(result);
+
+// let product = (a,b) => a*b;
+// console.log(product(14,5));
